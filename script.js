46175feb189eccd98f562da6b34e09a5bf5b33c3
Andrew Yeh <ayeh2317@gmail.com>
2022-06-02 20:36:30 -0700

    more tags and variables in script

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
 const options = {
-	method: 'GET'
+method: 'GET'
 };
 
-const url = 'https://app.ticketmaster.com//discovery/v2/events.json';
+const url = 'https://app.ticketmaster.com/discovery/v2/events.json';
 const apiKey = `&apikey=cs6ybE2gX1EZMGEsKgTr6gBTb75xbSQf`
 const keywordTag = '?keyword=';
+const radiusTag = '?radius=';
+const unitTag = '?unit=miles';
+const postalCodeTag = '?postalCode=';
+const cityTag = '?city=';
+let city = 'Modesto';
+let postalCode = 94102;
+let radius = 1000;
 let keyword = 'said the sky';
 
-fetch(url + keywordTag + keyword + apiKey, options)
-	.then(function (response) {
+let zipCode = 0;
+
+function getEvent() {
+    fetch(url + cityTag + city + apiKey, options)
+    .then(function (response) {
+        console.log(response)
         return response.json()
     })
-	.then(data => console.log(data._embedded.events));
+    .then(function(data) {
+        console.log(data)
+    });
+}
+
+getEvent();
