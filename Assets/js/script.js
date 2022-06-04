3f3cc2ab1e3c29a142147903f1c6c411b74e3ae2
Jmonty94 <103618671+jmonty94@users.noreply.github.com>
2022-06-04 10:22:13 -0700

    work on slider output

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -10,13 +10,15 @@ const unitTag = '&unit=miles';
 const postalCodeTag = '&postalCode=';
 const cityTag = '&city=';
 const latlongTag= '&latlong=';
+const rangeVal = document.getElementById("rangeVal")
+// const 
 let city = 'Modesto';
 let postalCode = 94102;
 let radius = 1000;
 let keyword = 'said the sky';
 let latlon = `37.6762348,-120.9362981`;
-
 let zipCode = 0;
+// rangeVal.textContent = rangeVal.value
 
 function getLocation() {
     if (navigator.geolocation) {
